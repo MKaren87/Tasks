c5a83e3dbdd2d69094d3ebc20aa1fd05d4a0f2b3
MKaren87 <19karen87@bk.ru>
2025-06-30 22:01:48 +0400

    Delete average.py

--- average.py
+++
@@ -1,8 +0,0 @@
-def average(*args):
-    numbers = [arg for arg in args if isinstance(arg, (int, float))]  
-    return sum(numbers) / len(numbers) if numbers else None  
-
-print(average(10, 20, 30))  
-print(average(4, "Python", 8, 12))  
-print(average())
-
