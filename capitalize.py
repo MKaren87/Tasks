bda1a44b74ab0f1b0cf6aa1bc476a5c0f710ffa8
MKaren87 <19karen87@bk.ru>
2025-06-30 22:01:24 +0400

    Delete capitalize.py

--- capitalize.py
+++
@@ -1,5 +0,0 @@
-def capitalize_words_ascii(s):
-    return ' '.join(chr(ord(word[0]) - 32) + word[1:] if 'a' <= word[0] <= 'z' else word for word in s.split())
-
-print(capitalize_words_ascii("hello world! python is awesome"))
-
