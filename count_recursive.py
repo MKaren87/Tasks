f2b79ed74cc97a680448634e8fc17247f3efe07b
MKaren87 <19karen87@bk.ru>
2025-08-20 00:45:18 +0400

    Delete count_recursive.py

--- count_recursive.py
+++
@@ -1,8 +0,0 @@
-def count_recursive(arr):
-    if not arr:  
-        return 0
-    return 1 + count_recursive(arr[1:])  
-
-numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-print(count_recursive(numbers))  
-
